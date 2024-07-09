f60d593ae7a6247932870405e82e21558398c550
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 <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>
2024-07-09 16:13:30 -0700

    Combine

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,10 @@
+export default function updateStudentGradeByCity(arr, city, newGrades) {
+  return arr
+    .filter((st) => st.location === city)
+    .map((st) => Object.assign(st,
+      {
+        grade: newGrades.filter((grade) => st.id === grade.studentId)[0]
+          ? newGrades.filter((grade) => st.id === grade.studentId)[0].grade : 'N/A',
+      }));
+}
+  
