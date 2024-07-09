2aa52b75fad49cfd5b5fdc5ba5b8f07bdb1334c7
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 <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>
2024-07-08 20:18:28 -0700

    lists all documents in a collection

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -7,7 +7,7 @@ import pymongo
 
 def list_all(mongo_collection):
     """
-    function that lists all documents in a collection
+    function that  lists all documents in a collection
     """
     listadocs = mongo_collection.find()
     if listadocs is None:
