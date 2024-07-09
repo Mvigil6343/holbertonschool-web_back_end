8868315965ee59ae4bfd2c769a072b8a9fc78c63
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 <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>
2024-07-08 20:26:02 -0700

    Change school topics

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -5,8 +5,8 @@ function that changes all topics of a school document based on the name
 import pymongo
 
 
-def update(mognocollection, name, topics):
+def update_topics(mongo_collection, name, topics):
     """
     function that changes all topics of a school document based on the name
     """
-    mognocollection.update_many({"name": name}, {"$set": {"topics": topics}})
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
