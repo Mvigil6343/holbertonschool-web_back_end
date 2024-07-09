2b5703e611d46ef115e6471f5214cb0a8fae1dca
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 <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>
2024-07-09 16:21:31 -0700

    lint fix

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,10 +1,8 @@
 export default function updateStudentGradeByCity(arr, city, newGrades) {
   return arr
     .filter((st) => st.location === city)
-    .map((st) => Object.assign(st,
-      {
-        grade: newGrades.filter((grade) => st.id === grade.studentId)[0]
-          ? newGrades.filter((grade) => st.id === grade.studentId)[0].grade : 'N/A',
-      }));
+    .map((st) => Object.assign(st, {
+      grade: newGrades.filter((grade) => st.id === grade.studentId)[0]
+        ? newGrades.filter((grade) => st.id === grade.studentId)[0].grade : 'N/A',
+    }));
 }
-  
