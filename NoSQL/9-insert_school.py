74a7a11a96a2559848e374d7bdcc0c016022cbe1
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 <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>
2024-07-08 20:20:32 -0700

    insert a new document in a collection

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -5,7 +5,7 @@ function that inserts a new document in a collection based on kwargs
 import pymongo
 
 
-def insert(mongo_collection, **kwargs):
+def insert_school(mongo_collection, **kwargs):
     """
     function that inserts a new document in a collection based on kwargs
     """
